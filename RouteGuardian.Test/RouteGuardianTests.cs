1f9763d769bede597717d56e6e14c946cd50a1d0
MiSeeger <miseeger@gmx.de>
2022-12-13 22:25:04 +0100

    Usings corrected

--- RouteGuardian.Test/RouteGuardianTests.cs
+++ RouteGuardian.Test/RouteGuardianTests.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
 using RouteGuardian.Model;
 
 namespace RouteGuardian.Test
--- RouteGuardian/Helper/WinHelper.cs
+++ RouteGuardian/Helper/WinHelper.cs
@@ -1,10 +1,7 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Security.Principal;
-using System.Text;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 
 namespace RouteGuardian.Helper
 {
